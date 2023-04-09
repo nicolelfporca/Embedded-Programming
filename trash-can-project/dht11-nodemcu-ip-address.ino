64aeea7e62f1b03a3fa1314416580bee83b96929
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2023-04-09 22:16:10 +0800

    Update dht11-nodemcu-ip-address.ino

--- trash-can-project/dht11-nodemcu-ip-address.ino
+++ trash-can-project/dht11-nodemcu-ip-address.ino
@@ -1,107 +1,57 @@
 // same dht11 wiring
 
-const int refresh = 3;  //3 seconds
-
-// DHT11
-#include "DHT.h"
-#define DHTPIN 2  // digital pin = D4
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-float tValue;
-
-// NODEMCU ESP8266
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+#include <DHT.h>
 
-#ifndef STASSID
-#define STASSID "SanaAllMayWifi"  //Your WiFi SSID
-#define STAPSK "banada2020"       //Your WiFi Password
-#endif
-
-const char* ssid = STASSID;
-const char* password = STAPSK;
+const char* ssid = "Kypen";
+const char* password = "Kyr13St3ph3n@";
 
 ESP8266WebServer server(80);
 
-void sendTemp() {
-  String page = "<!DOCTYPE html>\n\n";
-  page += "<meta http-equiv='refresh' content='";
-  page += String(refresh);  //how often temperature is read
-  page += "'/>\n";
-  page += "<html>\n";
-  page += "<body>\n";
-  page += "<p style=\"font-size:50px;\">Temperature<br/>\n";
-  page += "<p style=\"color:red; font-size:50px;\">";
-  page += String(tValue, 2);
-  page += " C </p>\n";
-  page += "</body>\n";
-  page += "</html>\n";
-  server.send(200, "text/html", page);
+#define DHTPIN 2       // D4 in nodemcu esp8266 pin
+#define DHTTYPE DHT11  // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void handleRoot() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  String html = "<html><body>";
+  html += "<h1>Temperature and Humidity readings:</h1>";
+  html += "<p>Temperature: " + String(temperature) + "C</p>";
+  html += "<p>Humidity: " + String(humidity) + "%</p>";
+  html += "</body></html>";
+  
+  server.send(200, "text/html", html);
 }
 
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
-
-void setup(void) {
-  dht.begin();
+void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
 
-  // Wait for connection
+  // Wait for WiFi connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  if (WiFi.status() == WL_CONNECTED) {
-    uint8_t macAddr[6];
-    WiFi.macAddress(macAddr);
-    Serial.printf("Connected, mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
-  }
+
   Serial.println("");
-  Serial.print("Connected to ");
+  Serial.println("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("robojaxDHT")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", sendTemp);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
+  server.on("/", handleRoot);
 
   server.begin();
   Serial.println("HTTP server started");
+  
+  dht.begin();
 }
 
-void loop(void) {
+void loop() {
   server.handleClient();
-  MDNS.update();
-  float celcius = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  float farenheit = dht.readTemperature(true);
-  Serial.println((String)celcius + " C ");
-  tValue = celcius;
-  delay(300);  // change this to larger value (1000 or more) if you don't need very often reading
 }
